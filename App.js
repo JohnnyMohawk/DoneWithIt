b59454efe2b63565ee6f2263276eb1496eadd403
John Nelson-Alden <75339192+JohnnyMohawk@users.noreply.github.com>
2022-12-04 21:21:29 -0600

    import and test modules

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TouchableWithoutFeedback, TouchableOpacity, TouchableHighlight, Alert, StatusBar, Platform, Image, SafeAreaView } from 'react-native';
 
 export default function App() {
+  let num = 0;
+  // const handlePress = () => {
+  //   num = num + 1
+  //   alert(num);
+  // };
+  const handlePress = () => {
+    Alert.prompt("Cheese", "Eat It!!!", text => console.log(text))
+  }  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text onPress={handlePress}>{num}</Text>
+      <TouchableHighlight onPress={handlePress}>
+        <Image source={{ 
+            width: 200,
+            height: 300,
+            uri: "https://picsum.photos/200/300" 
+          }} 
+        />
+
+      </TouchableHighlight>
+    </SafeAreaView>
   );
 }
 
@@ -14,7 +30,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
 });
